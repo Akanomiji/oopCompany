7e7f2c6f7264db8f93db51a6c56b42418c4106cb
Akanomiji <65529266+Akanomiji@users.noreply.github.com>
2024-08-28 09:55:28 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/oopcompany/OopCompany.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oopcompany;
+
+/**
+ *
+ * @author com4936
+ */
+public class OopCompany {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
